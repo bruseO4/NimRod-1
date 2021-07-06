6af4c233622409272e0efb148886d584553f52c5
bruhmoment00 <tredotcom2005@gmail.com>
2021-07-06 15:56:39 -0500

    Co-authored-by: arisharma43 <arisharma43@users.noreply.github.com> Co-authored-by: bruseO4 <bruseO4@users.noreply.github.com>

---
+++ logit_bias2.py
@@ -0,0 +1,23 @@
+logit_bias_2={
+   7635: -100, #style
+   12048:-100, #script
+   198: -100, #"enter"
+   220: -70, #"space"
+   14202: -100, #None
+   6494: -50, #html
+   7785: -100, #var
+   2618: -50, #body
+   #positive
+   16539: 5, #button
+   9600: 3, #img
+   5898: 5, #foot
+   15414: 5, #input
+   11840: 5, #hr
+   18242: 5, #label
+   4906: 5, #type
+   11487: 5, #table
+   18076: 5, #option
+   1640: 5, #for
+   377: 5, #ul
+   28341: 5, #nav
+}
